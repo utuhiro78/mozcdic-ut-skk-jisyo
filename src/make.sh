2cbf5b4652ab0f253880258af4aeaf3dd9d7ae09
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-01-26 06:17:01 +0900

    Sync

--- src/make.sh
+++ src/make.sh
@@ -7,6 +7,3 @@ ruby convert_skk_jisyo_to_mozcdic.rb
 
 tar cjf mozcdic-ut-skk-jisyo.txt.tar.bz2 mozcdic-ut-skk-jisyo.txt
 mv mozcdic-ut-skk-jisyo.txt* ../
-
-rm -rf ../../mozcdic-ut-skk-jisyo-release/
-rsync -a ../* ../../mozcdic-ut-skk-jisyo-release --exclude=SKK-JISYO* --exclude=mozcdic-ut-*.txt
